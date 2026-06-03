31f86fcd58b691d50cf1777fb5d14bd25bc20449
Pedrop662 <peamaral23ig@epms.pt>
2026-06-03 19:59:23 +0000

    update

--- mockup/vite.config.ts
+++ mockup/vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
-<<<<<<< HEAD
-import { fileURLToPath } from 'node:url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-=======
 
 // Figma asset resolver (mantido)
->>>>>>> 29b2b88 (update)
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
@@ -32,15 +24,9 @@ export default defineConfig({
 
   resolve: {
     alias: {
-<<<<<<< HEAD
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-=======
       '@': path.resolve(process.cwd(), './src'),
     },
   },
 
->>>>>>> 29b2b88 (update)
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
